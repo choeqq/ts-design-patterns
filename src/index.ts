0f1c73e99111eac79e2575928d33ed8b50eebbbf
choeqq <k.chola@op.pl>
2022-09-29 21:11:18 +0200

    observer pattern

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
 import { RecordHandler, loader } from "./loader";
 
+// Observer pattern
+type Listener<EventType> = (ev: EventType) => void;
+function createObserver<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  let listeners: Listener<EventType>[] = [];
+
+  return {
+    subscribe: (listener: Listener<EventType>): (() => void) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter((l) => l !== listener);
+      };
+    },
+    publish: (event: EventType) => {
+      listeners.forEach((l) => l(event));
+    },
+  };
+}
+
+interface BeforeSetEvent<T> {
+  value: T;
+  newValue: T;
+}
+
+interface AfterSetEvent<T> {
+  value: T;
+}
+
 interface Pokemon {
   id: string;
   attack: number;
@@ -13,6 +43,9 @@ interface BaseRecord {
 interface Database<T extends BaseRecord> {
   set(newValue: T): void;
   get(id: string): T | undefined;
+
+  onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+  onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
 }
 
 // Factory pattern
@@ -20,24 +53,54 @@ function createDatabase<T extends BaseRecord>() {
   class InMemoryDatabase implements Database<T> {
     private db: Record<string, T> = {};
 
+    private beforeAddListeners = createObserver<BeforeSetEvent<T>>();
+    private afterAddListeners = createObserver<AfterSetEvent<T>>();
+
     public set(newValue: T): void {
+      this.beforeAddListeners.publish({
+        newValue,
+        value: this.db[newValue.id],
+      });
+
       this.db[newValue.id] = newValue;
+
+      this.afterAddListeners.publish({
+        value: newValue,
+      });
     }
     public get(id: string): T {
       return this.db[id];
     }
+
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void {
+      return this.beforeAddListeners.subscribe(listener);
+    }
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void {
+      return this.afterAddListeners.subscribe(listener);
+    }
   }
 
-  // Singleton
+  // Singleton pattern
   const db = new InMemoryDatabase();
   return db;
 }
 
 const pokemonDB = createDatabase<Pokemon>();
+
 pokemonDB.set({
   id: "Bulbosaur",
   attack: 50,
   defense: 10,
+});
+
+pokemonDB.onAfterAdd(({ value }) => {
+  console.log(value);
+});
+
+pokemonDB.set({
+  id: "Pikachu",
+  attack: 130,
+  defense: 30,
 });
 
 console.log(pokemonDB.get("Bulbosaur"));
